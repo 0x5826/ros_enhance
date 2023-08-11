0066fc161018fa308849f6c19bdc0267174fb66a
0x5826 <47938087+0x5826@users.noreply.github.com>
2023-08-12 06:32:37 +0800

    add private address

--- ros_chnroute.sh
+++ ros_chnroute.sh
@@ -6,6 +6,9 @@ chnroute_list=$(curl -sSL https://raw.githubusercontent.com/mayaxcn/china-ip-lis
 
 echo "/ip firewall address-list remove [/ip firewall address-list find list=$listname]" > chnroute.rsc
 echo "/ip firewall address-list" >> chnroute.rsc
+echo "add address=10.0.0.0/8 disabled=no list=$listname" >> chnroute.rsc
+echo "add address=172.16.0.0/12 disabled=no list=$listname" >> chnroute.rsc
+echo "add address=192.168.0.0/16 disabled=no list=$listname" >> chnroute.rsc
 
 for chnroute_ip in $chnroute_list
 do
