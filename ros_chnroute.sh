348456957a2da1b5d51b5a25226bddbe27a1dcf5
dante <dante@seckv.com>
2025-01-15 05:52:19 +0000

    rename chnroute to chnroute_v4

--- ros_chnroute.sh
+++ ros_chnroute.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-listname="chnroute"
+listname="chnroute_v4"
 
 chnroute_list=$(curl -sSL https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt)
 
-echo "/ip firewall address-list remove [/ip firewall address-list find list=$listname]" > chnroute.rsc
-echo "/ip firewall address-list" >> chnroute.rsc
-echo "add address=10.0.0.0/8 disabled=no list=$listname" >> chnroute.rsc
-echo "add address=172.16.0.0/12 disabled=no list=$listname" >> chnroute.rsc
-echo "add address=192.168.0.0/16 disabled=no list=$listname" >> chnroute.rsc
+echo "/ip firewall address-list remove [/ip firewall address-list find list=$listname]" > chnroute_v4.rsc
+echo "/ip firewall address-list" >> chnroute_v4.rsc
+#echo "add address=10.0.0.0/8 disabled=no list=$listname" >> chnroute_v4.rsc
+#echo "add address=172.16.0.0/12 disabled=no list=$listname" >> chnroute_v4.rsc
+#echo "add address=192.168.0.0/16 disabled=no list=$listname" >> chnroute_v4.rsc
 
-for chnroute_ip in $chnroute_list
+for chnroute_ipv4 in $chnroute_list
 do
-    echo "add address=$chnroute_ip disabled=no list=$listname" >> chnroute.rsc
+    echo "add address=$chnroute_ipv4 disabled=no list=$listname" >> chnroute_v4.rsc
 done
